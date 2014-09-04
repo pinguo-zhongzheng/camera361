21b0d337a73e46d736969ae3511c51b62a05a954
zhongzheng12 <zhongzheng@camera360.com>
2014-09-04 17:32:13 +0800

    update some function of camera

--- app/src/main/java/pg/com/camera361/MainActivity.java
+++ app/src/main/java/pg/com/camera361/MainActivity.java
@@ -1,5 +1,6 @@
 package pg.com.camera361;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -7,12 +8,20 @@ import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -20,14 +29,51 @@ import java.util.Date;
 public class MainActivity extends ActionBarActivity {
 
     public static final int MEDIA_TYPE_IMAGE = 1;
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null) {
+                Log.d("Camera361", "Error creating media file, check storage permissions!");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d("Camera361", "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d("Camera361", "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
     public static final int MEDIA_TYPE_VIDEO = 2;
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
     private Camera mCamera;
     private CameraPreview mPreview;
+    private int numberOfCameras;
+    private int cameraCurrentlyLocked;
 
-
+    // The first rear facing camera
+    private int defaultCameraId;
     private Uri fileUri;
+
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
 
     /**
      * Create a file Uri for saving an image or video
@@ -51,7 +97,7 @@ public class MainActivity extends ActionBarActivity {
         // Create the storage directory if it does not exist
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
-                Log.d("MyCameraApp", "failed to create directory");
+                Log.d("Camera361", "failed to create directory");
                 return null;
             }
         }
@@ -72,41 +118,47 @@ public class MainActivity extends ActionBarActivity {
         return mediaFile;
     }
 
-    /**
-     * A safe way to get an instance of the Camera object.
-     */
-    public static Camera getCameraInstance() {
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        } catch (Exception e) {
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.camera_preview);
 
-        // Create an instance of Camera
-//        mCamera = getCameraInstance();
-//
-//        // Create our Preview view and set it as the content of our activity.
-//        mPreview = new CameraPreview(this, mCamera);
-//        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-//        preview.addView(mPreview);
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+        // Add a listener to the Capture button
+        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        mCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );
+        // Find the total number of cameras available
+        numberOfCameras = Camera.getNumberOfCameras();
+
+        // Find the ID of the default camera
+        Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.getCameraInfo(i, cameraInfo);
+            if (cameraInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                defaultCameraId = i;
+            }
+        }
 
 
 //        create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+//        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
 
         // start the image capture Intent
-        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+//        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 
         //create new Intent
 //        Intent intent2 = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
@@ -119,6 +171,79 @@ public class MainActivity extends ActionBarActivity {
         // start the Video Capture Intent
 //        startActivityForResult(intent2, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Open the default i.e. the first rear facing camera.
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+        cameraCurrentlyLocked = defaultCameraId;
+        mPreview.setCamera(mCamera);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Because the Camera object is a shared resource, it's very
+        // important to release it when the activity is paused.
+        if (mCamera != null) {
+            mPreview.setCamera(null);
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate our menu which can gather user input for switching camera
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.camera_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.switch_cam:
+                // check for availability of multiple cameras
+                if (numberOfCameras == 1) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage(this.getString(R.string.camera_alert))
+                            .setNeutralButton("Close", null);
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                    return true;
+                }
+
+                // OK, we have multiple cameras.
+                // Release this camera -> cameraCurrentlyLocked
+                if (mCamera != null) {
+                    mCamera.stopPreview();
+                    mPreview.setCamera(null);
+                    mCamera.release();
+                    mCamera = null;
+                }
+
+                // Acquire the next camera and request Preview to reconfigure
+                // parameters.
+                mCamera = Camera
+                        .open((cameraCurrentlyLocked + 1) % numberOfCameras);
+                cameraCurrentlyLocked = (cameraCurrentlyLocked + 1)
+                        % numberOfCameras;
+                mPreview.switchCamera(mCamera);
+
+                // Start the preview
+                mCamera.startPreview();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -160,6 +285,4 @@ public class MainActivity extends ActionBarActivity {
             return false;
         }
     }
-
-
 }
